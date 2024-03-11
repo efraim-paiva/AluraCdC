e9f4d7c06bd4c8d128597ffe354f6a770166c0c8
efraim-paiva <efgaroto@gmail.com>
2024-03-11 19:29:49 -0300

    Remove hardcoded product from cart and dynamically add products

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@ function adicionar() {
     var valorProduto = produto.split('R$')[1];
     var quantidade = document.getElementById('quantidade').value;
     var subTotal = valorProduto * quantidade;
-    var carrinho = document.getElementById('listaCarrinho');
+    var carrinho = document.getElementById('lista-produtos');
     var total = document.getElementById('valorTotal');
 
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto"><span class="texto-azul"> ${quantidade} </span> ${nomeProduto} <span class="texto-azul">${subTotal}</span></section>`
 }
 
 function limpar() {
